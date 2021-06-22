d117f49f2137ab8628444bc74f9f9e61ef6cc0f5
duaren <renaudduaren@gmail.com>
2021-06-22 17:11:37 +0100

    color flipper prject done

--- javascript-projects/01-color-flipper/app.js
+++ javascript-projects/01-color-flipper/app.js
@@ -1,1 +1,14 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "yellow", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    // nombre aléatoire entre 0 et 4 
+    const randomNumber = getRandomNumber() ;
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
