7a1f62c09f61bd617fd7564e847f598dec00bcbc
duaren <renaudduaren@gmail.com>
2021-06-22 18:12:13 +0100

    counter js project done

---
+++ javascript-projects/02-counter/app.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+ // selection des boutons
+ const value = document.getElementById("value");
+ const btns = document.querySelectorAll(".btn");
+
+ btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        }
+        else if (styles.contains("increase")) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+        value.textContent = count;
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if (count < 0) {
+            value.style.color = "red";
+        }
+        else  {
+            value.style.color = "#222";
+        }
+    });
+ });
