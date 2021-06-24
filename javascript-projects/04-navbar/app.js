feaddc2c23b2191621699cd7ad99387030d27788
duaren <renaudduaren@gmail.com>
2021-06-24 18:44:02 +0100

    navbar js projects done

--- javascript-projects/04-navbar/app.js
+++ javascript-projects/04-navbar/app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    /* Toggle with if-else funtion
+    if (links.classList.contains("show-links")) {
+        links.classList.remove("show-links");
+    }
+    else {
+        links.classList.add("show-links");
+    } */
+
+    // Toggle with toggle property
+    links.classList.toggle("show-links");
+});
